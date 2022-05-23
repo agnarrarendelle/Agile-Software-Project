25e79d6d9fd548d05b36e9e9b628d65fc1bf76c6
Matt <danny20001102@gmail.com>
2022-05-23 16:47:09 +0800

    ListItem comments done

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,46 +1,54 @@
 import React, { Component } from "react";
 import "./ListItem.css";
 
+//This Interface defines properties passed to component ListItem
 interface Props {
   id: string;
   title: string;
   isDone: boolean;
   addedTime: string;
+  //Called in HandleChange when the check box of a task has been clicked
   checkTodo: (id: string, isDone: boolean) => void;
+  //Called in handleClick when user click on delete bottom next to a task
   deleteTodo: (id: string) => void;
 }
 
-export default class ListItem extends Component<Props> {
-  handleChange = (id: string) => {
-    return (event: React.ChangeEvent<HTMLInputElement>) => {
-      this.props.checkTodo(id, event.target.checked);
-    };
+class ListItem extends Component<Props> {
+  //Triggered when the check box of a task has been clicked
+  //The function calls the checkTodo function in App component
+  //with the id of the task and the boolean value that represent whether the check box is marked
+  //to update the boolean value of the task clicked in the Todo List
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.props.checkTodo(this.props.id, event.target.checked);
   };
 
-  handleClick = (id: string) => {
-    return () => {
-      if (window.confirm("Are you sure you want to delete this task?")) {
-        this.props.deleteTodo(id);
-      }
-    };
+  //Triggered when the user click on delete bottom next to a task
+  //The function first pops a window asking whether the user really wants to delete the task
+  //And if the user click ok, it would call deleteTodo function in App component
+  //with the id of that task to remove the task from the Todo List
+  handleClick = () => {
+    if (!window.confirm("Are you sure you want to delete this task?")) return;
+    this.props.deleteTodo(this.props.id);
   };
 
   render() {
-    const { id, title, isDone, addedTime } = this.props;
+    const { title, isDone, addedTime } = this.props;
     return (
       <li>
         <label>
           <input
             type="checkbox"
             defaultChecked={isDone}
-            onChange={this.handleChange(id)}
+            onChange={this.handleChange}
           />
           <span>{`   ${title}  ${addedTime}`}</span>
         </label>
-        <button className="btn btn-danger" onClick={this.handleClick(id)}>
+        <button className="btn btn-danger" onClick={this.handleClick}>
           Delete
         </button>
       </li>
     );
   }
 }
+
+export default ListItem;
