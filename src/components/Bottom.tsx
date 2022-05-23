495893e926ef0747ca26e1a5731d6898e6f7227a
Matt <danny20001102@gmail.com>
2022-05-23 22:56:52 +0800

    Bottom comments done

--- src/components/Bottom.tsx
+++ src/components/Bottom.tsx
@@ -1,43 +1,55 @@
 import React, { Component } from "react";
 import "./Bottom.css";
 
+//This Interface defines properties passed to component Bottom
 interface Props {
+  //An array of todo tasks that will be displayed
   todos: Array<TodoObJ>;
+  //Event handler defined in App component that would change the isDone field of all tasks to the opposite
   checkAllTodo: (isDone: boolean) => void;
+  //Event handler defined in App component that remove all tasks in the Todo List
   clearAllTodoDone: () => void;
 }
 
 export default class Footer extends Component<Props> {
+  //Triggered when users click the check box of the Bottom
+  //it would then checkAllTodo in App componet with the boolean value of the check box
   handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     this.props.checkAllTodo(event.target.checked);
   };
-
+  //Triggered when users click 'Clear Finished Tasks' bottom
+  //it would then checkAllTodo in App componet to remove all tasks that are finished
   handleClick = () => {
     this.props.clearAllTodoDone();
   };
 
   render() {
     const { todos } = this.props;
-    const doneTotal = todos.reduce(
-      (pre, todo) => pre + (todo.isDone ? 1 : 0),
+    //Iterate all tasks and count the number of finished tasks
+    const doneTasksNum = todos.reduce(
+      (prevTodo, currentTodo) => prevTodo + (currentTodo.isDone ? 1 : 0),
       0
     );
-    const total = todos.length;
+    const totalTasksNum = todos.length;
 
     return (
       <div
         className="todo-bottom"
-        style={{ display: total ? "block" : "none" }}
+        //if there is there is at least one task, display the Bottom component
+        style={{ display: totalTasksNum ? "block" : "none" }}
       >
         <label>
           <input
             type="checkbox"
-            checked={doneTotal === total && total > 0}
+            //set checked to true if
+            //there is at least one task in the Todo List
+            //and the number of finished task is equal to the number of all tasks
+            checked={totalTasksNum > 0 && doneTasksNum === totalTasksNum  }
             onChange={this.handleChange}
           />
         </label>
         <span>
-          <span>Finished:{doneTotal}</span> / All:{total}
+          <span>Finished:{doneTasksNum}</span> / All:{totalTasksNum}
         </span>
         <button className="btn btn-danger" onClick={this.handleClick}>
           Clear Finished Tasks
