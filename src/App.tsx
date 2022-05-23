5748c03ff1b0d31cc3899174b83cd713ce1f6d17
Matt <danny20001102@gmail.com>
2022-05-23 23:36:15 +0800

    App comments done

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import List from "./components/List";
 import Bottom from "./components/Bottom";
 import "./App.css";
 
+//This Interface defines the states that App component will maintain
 interface States {
+  //An array of todo tasks that will be modified and updated based on user's actions
   todos: Array<TodoObJ>;
 }
 
 class App extends Component<any, States> {
+  //In constructor, initialize the todos to an empty array
   constructor(props: any) {
     super(props);
     this.state = {
@@ -16,6 +19,9 @@ class App extends Component<any, States> {
     };
   }
 
+  //Added task to the todo array
+  //When called, it would add the new TodoObj to the a copy of todos array
+  //then call setState with the new array to re-render the page
   addTodo = (todoObj: TodoObJ) => {
     const { todos } = this.state;
     const newTodos = [todoObj, ...todos];
@@ -23,7 +29,13 @@ class App extends Component<any, States> {
       todos: newTodos,
     });
   };
-
+  //Modified the isDone boolean value of a TodoObj in todo array
+  //When called, it would iterate through all TodoObj in the todo array
+  //and if the current TodoObj has the same ID as the argument ID
+  //it would make a copy of that TodoObj and set its isDone value to the argument isDone
+  //But if the id is not the same, it would simpy return that TodoObj
+  //After the iteration, an new array storing the old TodoObjs and modified TodoObj is created
+  //then it would call setState with the new array to re-render the page
   checkTodo = (id: string, isDone: boolean) => {
     const { todos } = this.state;
     const newTodos = todos.map((todoObj) => {
@@ -35,6 +47,11 @@ class App extends Component<any, States> {
     });
   };
 
+  //Remove a TodoObj from todos Array
+  //When called, it would first make a copy of the original todos array
+  //then it would iterate through the copy, checking if the id is the same as argument id
+  //If the id is not the same as argument id, it would remove that TodoObj from the copy array
+  //then it would call setState with the new array to re-render the page
   deleteTodo = (id: string) => {
     const { todos } = this.state;
     const newTodos = todos.filter((todo) => todo.id !== id);
@@ -43,13 +60,23 @@ class App extends Component<any, States> {
     });
   };
 
+  //Set all isDone value of TodoObjs in todos array to argument isDone
+  //When called, it would first make a copy of the original todos array
+  //then it would iterate through the copy, setting each isDone of each TodoObj to argument isDone
+  //then it would call setState with the new array to re-render the page
   checkAllTodo = (isDone: boolean) => {
     const { todos } = this.state;
     const newTodos = todos.map((todo) => {
       return { ...todo, isDone };
     });
-     this.setState({ todos: newTodos });
+    this.setState({ todos: newTodos });
   };
+
+  //remove the TodoObjs from todo array if their isDone field is true
+  //When called, it would first make a copy of the original todos array
+  //then it would iterate through the copy, checking the isDone value of each TodoObj
+  //and remove the current TodoObj if its isDone value is true
+  //then it would call setState with the new array to re-render the page
 
   clearAllTodoDone = () => {
     const { todos } = this.state;
