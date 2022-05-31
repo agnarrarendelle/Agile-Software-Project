64a7b68d022f86069b69aed9f5c1d83cc959e85b
Matt <danny20001102@gmail.com>
2022-05-31 15:54:59 +0800

    added a new componet to allow users to choose the priority level of a task

--- src/components/PriorityLevelOption.tsx
+++ src/components/PriorityLevelOption.tsx
@@ -2,23 +2,29 @@ import { Component } from "react";
 import './PriorityLevelOption.css'
 interface Props {
   proorityLevels: Array<PriorityLevelObj>;
+  setPriorityLevel:(newLEvel:string)=>void;
 }
 
 class PriorityLevelOption extends Component<Props> {
+
+    onOptionChange = (event:React.ChangeEvent<HTMLSelectElement>)=>{
+         console.log(event.target.value);
+         this.props.setPriorityLevel(event.target.value)
+    }
+
   getOptions = () => {
     const options = this.props.proorityLevels.map((level) => {
       return (
-        <option className={`level-${level.level}`} key={level.PriorityLevelName}>
+        <option value={level.level} className={`level-${level.level}`} key={level.PriorityLevelName}>
           {level.level}: {level.PriorityLevelName}
         </option>
       );
     });
-    const firstOption = <option key={'first option'}>--Please Select a Priority--</option>
-    return [firstOption].concat(options);
+    return options
   };
 
   render() {
-    return <select>{this.getOptions()}</select>;
+    return <select onChange={this.onOptionChange}>{this.getOptions()}</select>;
   }
 }
 
