ca0e19a7454350a22af0d50e54f59cdf15e88aa9
Matt1899 <danny20001102@gmail.com>
2022-08-06 22:52:56 +0800

    added three state, one to open the modal, one to set the modal title and one to set modal item id

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import List from "./components/List";
 import Bottom from "./components/Bottom";
 import SortOptions from "./components/SortOptions";
 import "./App.css";
+import Modal from "./components/Modal"
 
 //This Interface defines the states that App component will maintain
 interface States {
@@ -11,141 +12,25 @@ interface States {
   todos: Array<TodoObJ>;
 }
 
-// class App extends Component<any, States> {
-//   //In constructor, initialize the todos to an empty array
-//   constructor(props: any) {
-//     super(props);
-//     this.state = {
-//       todos: [],
-//     };
-//   }
-
-//   //Added task to the todo array
-//   //When called, it would add the new TodoObj to the a copy of todos array
-//   //then call setState with the new array to re-render the page
-//   addTodo = (todoObj: TodoObJ) => {
-//     const { todos } = this.state;
-//     const newTodos: Array<TodoObJ> = [todoObj, ...todos];
-//     this.setState({
-//       todos: newTodos,
-//     });
-//   };
-//   //Modified the isDone boolean value of a TodoObj in todo array
-//   //When called, it would iterate through all TodoObj in the todo array
-//   //and if the current TodoObj has the same ID as the argument ID
-//   //it would make a copy of that TodoObj and set its isDone value to the argument isDone
-//   //But if the id is not the same, it would simpy return that TodoObj
-//   //After the iteration, an new array storing the old TodoObjs and modified TodoObj is created
-//   //then it would call setState with the new array to re-render the page
-//   checkTodo = (id: string, isDone: boolean) => {
-//     const { todos } = this.state;
-//     const newTodos: Array<TodoObJ> = todos.map((todoObj) => {
-//       if (todoObj.id === id) return { ...todoObj, isDone: isDone };
-//       else return todoObj;
-//     });
-//     this.setState({
-//       todos: newTodos,
-//     });
-//   };
-
-//   //Remove a TodoObj from todos Array
-//   //When called, it would first make a copy of the original todos array
-//   //then it would iterate through the copy, checking if the id is the same as argument id
-//   //If the id is not the same as argument id, it would remove that TodoObj from the copy array
-//   //then it would call setState with the new array to re-render the page
-//   deleteTodo = (id: string) => {
-//     const { todos } = this.state;
-//     const newTodos: Array<TodoObJ> = todos.filter((todo) => todo.id !== id);
-//     this.setState({
-//       todos: newTodos,
-//     });
-//   };
-
-//   //Set all isDone value of TodoObjs in todos array to argument isDone
-//   //When called, it would first make a copy of the original todos array
-//   //then it would iterate through the copy, setting each isDone of each TodoObj to argument isDone
-//   //then it would call setState with the new array to re-render the page
-//   checkAllTodo = (isDone: boolean) => {
-//     const { todos } = this.state;
-//     const newTodos: Array<TodoObJ> = todos.map((todo) => {
-//       return { ...todo, isDone };
-//     });
-//     this.setState({ todos: newTodos });
-//   };
-
-//   //remove the TodoObjs from todo array if their isDone field is true
-//   //When called, it would first make a copy of the original todos array
-//   //then it would iterate through the copy, checking the isDone value of each TodoObj
-//   //and remove the current TodoObj if its isDone value is true
-//   //then it would call setState with the new array to re-render the page
-
-//   clearAllTodoDone = () => {
-//     const { todos } = this.state;
-//     const newTodos: Array<TodoObJ> = todos.filter((todo) => !todo.isDone);
-//     this.setState({ todos: newTodos });
-//   };
-
-//   sortTodo = (option: string) => {
-//     const newTodos = [...this.state.todos];
-//     switch (option) {
-//       case "date":
-//         newTodos.sort((a: TodoObJ, b: TodoObJ) => {
-//           return Date.parse(a.addedTime) - Date.parse(b.addedTime);
-//         });
-//         break;
-//       case "name":
-//         newTodos.sort((a: TodoObJ, b: TodoObJ) => {
-//           return a.title.localeCompare(b.title);
-//         });
-//         break;
-//       case 'priority level':
-//         newTodos.sort((a: TodoObJ, b: TodoObJ) => {
-//           return a.priorityLevel.localeCompare(b.priorityLevel);
-//         });
-//         break;
-//       default:
-//         break;
-//     }
-//     this.setState({ todos: newTodos });
-//   };
-
-//   render() {
-//     const { todos } = this.state;
-
-//     return (
-//       <div className="todo-container">
-//         <div className="todo-wrap">
-//           <TaskBar addTodo={this.addTodo} />
-//           <SortOptions
-//             options={["name", "date", "priority level"]}
-//             sortTodo={this.sortTodo}
-//           ></SortOptions>
-//           <List
-//             todos={todos}
-//             checkTodo={this.checkTodo}
-//             deleteTodo={this.deleteTodo}
-//           />
-//           <Bottom
-//             todos={todos}
-//             checkAllTodo={this.checkAllTodo}
-//             clearAllTodoDone={this.clearAllTodoDone}
-//           />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-function App() {
-  //   //In constructor, initialize the todos to an empty array
-  //   constructor(props: any) {
-  //     super(props);
-  //     this.state = {
-  //       todos: [],
-  //     };
-  //   }
-
+const App=()=> {
   const [todos, setTodos] = useState<Array<TodoObJ>>([]);
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [modalItemTitle,setModalItemTitle] = useState<string>("");
+  const [modalItemId,setModalItemId] = useState<string>("");
+
+  const openModalAndSetTitle=(title:string,id:string)=>{
+    setModalItemTitle(title)
+    setOpenModal(true)
+    setModalItemId(id)
+  }
+
+  const changeTitle = (newTitle:string, id:string)=>{
+    const newTodos = [...todos]
+    const target = newTodos.find(todo=>todo.id === id)
+    target!.title = newTitle
+    setTodos(newTodos)
+  }
+  
 
   //Added task to the todo array
   //When called, it would add the new TodoObj to the a copy of todos array
@@ -229,6 +114,8 @@ function App() {
   //   render() {
 
   return (
+    <div>
+
     <div className="todo-container">
       <div className="todo-wrap">
         <TaskBar addTodo={addTodo} />
@@ -236,13 +123,17 @@ function App() {
           options={["name", "date", "priority level"]}
           sortTodo={sortTodo}
         ></SortOptions>
-        <List todos={todos} checkTodo={checkTodo} deleteTodo={deleteTodo} />
+        <List todos={todos} checkTodo={checkTodo} deleteTodo={deleteTodo} openModalAndSetTitleId={openModalAndSetTitle}/>
         <Bottom
           todos={todos}
           checkAllTodo={checkAllTodo}
           clearAllTodoDone={clearAllTodoDone}
         />
       </div>
+      
+    </div>
+    {openModal && <Modal setOpenModal={setOpenModal} title={modalItemTitle} id={modalItemId} changeTodos={changeTitle}></Modal>}
+      {/* <button onClick={()=>{setOpenModal(true)}}>open</button> */}
     </div>
   );
   //   }
