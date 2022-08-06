67a68ec620e01d88053fd5ffd3af299f3b0fa678
Matt1899 <danny20001102@gmail.com>
2022-08-06 22:51:56 +0800

    deleted some comments

--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -13,55 +13,6 @@ interface State {
   priorityLevel: string;
 }
 
-// class TaskBar extends Component<Props,State> {
-//   state={
-//       priorityLevel:'1'
-//     }
-
-//   //Triggered when users enter anything in the TaskBar
-//   //First, it checks whether the Users hit Enter Key or do not enter text value
-//   //and return from the function if either is true
-//   //Second, it creates a new TodoObj and call addTodo function in App component
-//   //to add the new task to the Todo List
-//   //Finally, after the new task has been added to the Todo List
-//   //it would clear the TaskBar
-//   add = (event: React.KeyboardEvent<HTMLInputElement>) => {
-//     const { target, key } = event;
-//     let value = (target as HTMLTextAreaElement).value;
-//     if (key !== "Enter" || value.trim().length === 0) return;
-
-//     const todoObj: TodoObJ = {
-//       //get an unique ID with the nanoid library
-//       id: nanoid(),
-//       title: value,
-//       isDone: false,
-//       addedTime: new Date().toLocaleString(),
-//       priorityLevel:this.state.priorityLevel
-//     };
-//     this.props.addTodo(todoObj);
-
-//     (event.target as HTMLInputElement).value = "";
-//   };
-
-//   getPriorityLevel =(newLevel:string)=>{
-//     const newState = {priorityLevel:newLevel}
-//     this.setState(newState)
-//   }
-
-//   render() {
-//     return (
-//         <div className="task-bar">
-//           <input
-//             type="text"
-//             placeholder="Please enter your the name of your task"
-//             onKeyUp={this.add}
-//             />
-//             <PriorityLevelOption setPriorityLevel={this.getPriorityLevel} proorityLevels={[{level:1, PriorityLevelName:'urgent'}, {level:2, PriorityLevelName:'normal'},  {level:3, PriorityLevelName:'low'}]}></PriorityLevelOption>
-//         </div>
-//     );
-//   }
-// }
-
 function TaskBar(props: Props): React.ReactElement {
   const [priorityLevel, setPriorityLevel] = useState("1");
 
