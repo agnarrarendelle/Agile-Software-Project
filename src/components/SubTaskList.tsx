332b52fa7ded19dfbc3b3a6a5f045bf1ef50dbbd
Matt1899 <danny20001102@gmail.com>
2022-08-29 19:54:30 +0800

    added subtask list in modal

---
+++ src/components/SubTaskList.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./SubTaskList.css"
+interface Props {
+  subTasks: string[];
+  id: string;
+  addSubTasks: (taskName: string, id: string) => void;
+}
+
+const SubTaskList = (props: Props) => {
+  const [enteredTask, setEnteredTask] = useState("");
+
+  const onTaskNameChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const input = e.currentTarget.value.trim().toLowerCase();
+    console.log(input);
+    setEnteredTask(input);
+  };
+
+  const onButtonClicked = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    props.addSubTasks(enteredTask, props.id);
+  };
+
+  const getSubTasks = () => {
+    const tasks = props.subTasks.map((task) => {
+      return <li key={task}>{task}</li>;
+    });
+
+    return tasks
+  };
+
+  return (
+    <div className="sub-tasks">
+      <ol>{getSubTasks()}</ol>
+      <h5>Add a Sub Task</h5>
+      <input onChange={onTaskNameChange} className="" type="text" />
+
+      <button onClick={onButtonClicked}>Add</button>
+    </div>
+  );
+};
+
+export default SubTaskList;
